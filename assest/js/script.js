8b23765a1cf894e1bca2f90278dc4dc87e588072
Azhagu <azhagukarthikeyanb@gmail.com>
2024-08-22 19:02:06 +0530

    Added

---
+++ assest/js/script.js
@@ -0,0 +1,23 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        900:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    }
+})
